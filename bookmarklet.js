47f445b4250b456d2136749cc070ff9ab4458e4c
Jonathan Berger <pair+jonathanpberger@pivotallabs.com>
2012-03-16 14:31:20 -0400

    Added NYC managers and their reports. All NY managers should check that this works. (Especially those with apostrophes in their name.)

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,11 +1,19 @@
 javascript:(function (jQ) {
   var people_lists = {
+
     'Will Read':['Roberto Ostinelli', 'Charles Hansen', 'Max Brunsfeld', 'Stephen Hopkins', 'Mark Rushakoff', 'Aaron VonderHaar', 'Rachel Bobbins', 'Will Read'],
     'Davis Frank':[],
     'Jonathan Barnes':[],
     'David Goudreau':[],
     'Ryan Richard':[],
     'Danny Burkes':[],
+
+    'Jonathan Berger':['Steve Ellis', 'Daniel Fox', 'Robbie Clutton', 'Ryan Ong', 'Micah Young', 'Grant Hutchins', 'Jeff Saracco', 'Ben Moss', 'Dimitri Roche', 'Johnny Mukai'],
+		'Cathy O\'Connell':['Matthew Parker','Matt Royal','Dirk Kelly','John Barker','Nick Greenfield','Andy Pliszka','James Somers','David Goddard','Alex Kwiatkowski','Michael Freedman'],
+		'Josh Knowles':['Cathy O\'Connell','Kris Hicks','Jonathan Berger','Ian Zabel','Michael Schubert','Peter Jaros'],
+		'Ian Zabel':['Mark Gangl','Andrew Fader','Jake Quain','Brent Wheeldon','Luke Crawford','JT Archie','Evan Farrar','Lee Byrd','Adam Berlin','Sam Coward'],
+
+
   };
   var signed_in_manager = jQ('.welcome_message').text().replace('Welcome, ', '').trim();
   var people = people_lists[signed_in_manager];
